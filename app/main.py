3c776b033cf7de531db72ec05d65d3f3e7f3bb09
Jinsu Ha <korean_js@naver.com>
2022-11-29 16:27:24 +0900

    Feat: Add authentication provider - update readme - pymysql module install requirement

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 
 from .api.v1 import words, domain, terms
 
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app.include_router(words.router)
 app.include_router(domain.router)
@@ -11,7 +14,7 @@ app.include_router(terms.router)
 
 
 @app.get("/")
-async def index():
+async def index(token: str = Depends(oauth2_scheme)):
     return {"message": "Ok"}
 
 
